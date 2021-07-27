55f0ae44a37b2c2b9642f17700d45a2d071fa9c7
ameya1301999 <ameyarathi1301999@gmail.com>
2021-07-27 23:30:27 +0530

    commit

--- db/cookie.sql
+++ db/cookie.sql
@@ -4,16 +4,6 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
---
--- Database: `cookie`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
 CREATE TABLE `user` (
   `userid` int(11) NOT NULL,
   `username` varchar(30) NOT NULL,
@@ -21,24 +11,14 @@ CREATE TABLE `user` (
   `fullname` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `user`
---
 
 INSERT INTO `user` (`userid`, `username`, `password`, `fullname`) VALUES
 (1, 'LTI', '123', 'LTI'),
 (2, 'AMEYA', 'RATHI', 'AMEYA RATHI');
 
 
---
--- Indexes for table `user`
---
 ALTER TABLE `user`
   ADD PRIMARY KEY (`userid`);
 
-
---
--- AUTO_INCREMENT for table `user`
---
 ALTER TABLE `user`
   MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
